197d02a9d88145d267c2be4fdc02c9dc77842408
Jeff Bonyun <jbonyun@utexas.edu>
2022-03-29 23:11:44 -0500

    Expand graphical example to custom pose

--- graphical.m
+++ graphical.m
@@ -1,6 +1,12 @@
 % Graphical demonstration of the robot
 
-% A demonstration of the basics
+% Load the robot from the Matlab built-in set.
 kuka = loadrobot('kukaIiwa14');
+% Prepare an array of structs that can be used for the robot config
 config = randomConfiguration(kuka);
+% Replace the random joint angles with my own
+example_pose = joints(6, :);
+num2cell(deg2rad(example_pose));
+[config.JointPosition] = ans{:};
+% Display the resulting configuration.
 show(kuka, config);
