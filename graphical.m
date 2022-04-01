419d315ec6fabf944a59c69b7f8c05b853cb0670
Jeff Bonyun <jbonyun@utexas.edu>
2022-04-01 13:10:28 -0500

    Generate graphics for each of the test poses
    For including them in the report.

--- graphical.m
+++ graphical.m
@@ -5,11 +5,21 @@ test_kuka_examples;
 
 % Load the robot from the Matlab built-in set.
 kuka = loadrobot('kukaIiwa14');
+
 % Prepare an array of structs that can be used for the robot config
 config = randomConfiguration(kuka);
-% Replace the random joint angles with my own
-example_pose = joints(6, :);
-num2cell(deg2rad(example_pose));
-[config.JointPosition] = ans{:};
-% Display the resulting configuration.
-show(kuka, config);
+
+eg_poses = 1:16; %[1 2];
+for i = 1:numel(eg_poses)
+    % Replace the random joint angles with my own
+    eg_pose_num = eg_poses(i);
+    example_pose = joints(eg_pose_num, :);
+    num2cell(deg2rad(example_pose));
+    [config.JointPosition] = ans{:};
+    % Display the resulting configuration.
+    show(kuka, config);
+    title(sprintf('Test Config %d', eg_pose_num));
+    % Wait for input before continuing.
+    pause;
+end
+close;
