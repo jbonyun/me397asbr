093d7a6fdef7f3a380791296afe96909a091f266
Jeff Bonyun <jbonyun@utexas.edu>
2022-04-04 19:42:51 -0500

    Propagate symbolic types through unskewsym

--- unskewsym.m
+++ unskewsym.m
@@ -13,12 +13,12 @@ function vec = unskewsym(ss)
         assert(all(abs(triu(ss) - -triu(ss')) < 1e-9, 'all'), 'Not skew-symmetric because not negative-mirrored across diagonal');
         assert(all(diag(ss) == 0),'Not skew-symmetric because diagonal not 0');
     
-        vec = zeros(3,1);
+        vec = zeros(3,1,'like',ss);
         vec(1, 1) = ss(3, 2);
         vec(2, 1) = ss(1, 3);
         vec(3, 1) = ss(2, 1);
     elseif all(size(ss) == [4 4])
-        vec = zeros(6,1);
+        vec = zeros(6,1,'like',ss);
         vec(1:3) = unskewsym(ss(1:3, 1:3));
         vec(4:6) = ss(1:3, 4);
     else
