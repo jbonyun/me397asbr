cdb33e14a1a0704d2c86f90995d4dc7ca88277ab
Jeff Bonyun <jbonyun@utexas.edu>
2022-04-06 21:51:32 -0500

    Improve error messages in adjoint_transform

--- adjoint_transform.m
+++ adjoint_transform.m
@@ -7,8 +7,8 @@ function ad = adjoint_transform(trans)
     % Jeff Bonyun (jb79332), jbonyun@utexas.edu, 20220323
     % On behalf of the Sun/Bonyun team for ME397 ASBR, Spring 2022.
 
-    assert(size(trans,1) == 4);
-    assert(size(trans,2) == 4);
-    assert(all(trans(4,:) == [0 0 0 1], "all"));
+    assert(size(trans,1) == 4, 'Must have 4 rows to be transform');
+    assert(size(trans,2) == 4, 'Must have 4 columns to be transform');
+    assert(all(trans(4,:) == [0 0 0 1], "all"), 'Last col must be [0 0 0 1]');
 
     ad = [trans(1:3,1:3) zeros(3,3); skewsym(trans(1:3,4)) * trans(1:3,1:3) trans(1:3,1:3)];
