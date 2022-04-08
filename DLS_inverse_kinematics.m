3a0dbc795b2ed32c9b4831a23b3c1ff8691af4f4
Jeff Bonyun <jbonyun@utexas.edu>
2022-04-08 00:23:03 -0500

    Improve report making in DLS_inverse

--- DLS_inverse_kinematics.m
+++ DLS_inverse_kinematics.m
@@ -1,4 +1,4 @@
-function joint_angles = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
+function [joint_angles, iter_errang, iter_errlin, iter_cond, iter_stepnorm] = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
     % Solve inverse kinematics using DLS to avoid singularities.
     % Inputs:
     %   robot: struct with robot description
@@ -7,6 +7,7 @@ function joint_angles = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
     % Output:
     %    joint_angles: nx1 joint angles in radians that will acheive the
     %                  requested transformation.
+    %    iter_*: kx1 iteration-by-iteration progress of metrics.
     % Jeff Bonyun (jb79332), jbonyun@utexas.edu, 20220406
     % On behalf of the Sun/Bonyun team for ME397 ASBR, Spring 2022.
     % Source: Alambeigi, F. ASBR Lecture Notes. 2022, W10-L1 p. 9.
@@ -15,10 +16,11 @@ function joint_angles = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
     %#ok<*MINV> 
 
     % Some constants for the algorithm.
-    lr = 1;  % Learning rate; how fast we descend along gradient. Notes say "1".
+    lr = 0.5;  % Learning rate; how fast we descend along gradient. Notes say "1".
     angular_thresh = 0.001;  % When to stop the search.
     linear_thresh = 0.01;  % When to stop the search.
     k = 0.01;
+    do_print = true;
 
     % Helper lambda for getting the twist.
     get_twist = @(joint_angles) get_twist_ex(robot, joint_angles, Tsd);
@@ -28,8 +30,14 @@ function joint_angles = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
 
     iter = 0;
     twist_b = get_twist(joint_angles);
-    cond_num = J_condition(J_body(robot, joint_angles));
-    fprintf('Iter %d  StepNorm: %8f  ErrAng %9f  ErrLin %9f  Cond %12.2f  Angles %s\n', iter, nan, norm(twist_b(1:3)), norm(twist_b(4:6)), cond_num, mat2str(joint_angles'));
+    iter_errang(iter+1) = norm(twist_b(1:3));
+    iter_errlin(iter+1) = norm(twist_b(4:6));
+    iter_cond(iter+1) = J_condition(J_body(robot, joint_angles));
+    iter_stepnorm(iter+1) = nan;
+    if do_print
+        fprintf('%4s  %8s  %9s  %10s  %12s  %s\n', 'Iter', 'StepNorm', 'ErrAng', 'ErrLin', 'Cond#', 'Joint Angles');    
+        fprintf('%4d  %8f  %9f  %10f  %12.2f  %s\n', iter, iter_stepnorm(iter+1), iter_errang(iter+1), iter_errlin(iter+1), iter_cond(iter+1), mat2str(joint_angles', 5));
+    end
     while norm(twist_b(1:3)) > angular_thresh || norm(twist_b(4:6)) > linear_thresh
         iter = iter + 1;
         % Calculate step
@@ -40,10 +48,14 @@ function joint_angles = DLS_inverse_kinematics(robot, guess_joint_angles, Tsd)
         joint_angles = mod(joint_angles + step, 2*pi);
         % Prepare state for next iteration
         twist_b = get_twist(joint_angles);
-        % Report on progress
-        if mod(iter, 1) == 0
-            cond_num = J_condition(Jb);
-            fprintf('Iter %d  StepNorm: %8f  ErrAng %9f  ErrLin %9f  Cond %12.2f  Angles %s\n', iter, norm(step), norm(twist_b(1:3)), norm(twist_b(4:6)), cond_num, mat2str(joint_angles'));
+        % Save progress
+        iter_errang(iter+1) = norm(twist_b(1:3));
+        iter_errlin(iter+1) = norm(twist_b(4:6));
+        iter_cond(iter+1) = J_condition(Jb);
+        iter_stepnorm(iter+1) =  norm(step);
+        % Print progress
+        if do_print && mod(iter, 1) == 0
+            fprintf('%4d  %8f  %9f  %10f  %12.2f  %s\n', iter, iter_stepnorm(iter+1), iter_errang(iter+1), iter_errlin(iter+1), iter_cond(iter+1), mat2str(joint_angles', 4));
         end
     end
 end
