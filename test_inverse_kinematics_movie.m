a9f9e4d23372ce5ed7315b539422df1a3f8fbbb9
Jeff Bonyun <jbonyun@utexas.edu>
2022-04-19 09:59:37 -0500

    Use bigger ridge adjustment in DLS
    Makes it more different from other methods, for demonstration

--- test_inverse_kinematics_movie.m
+++ test_inverse_kinematics_movie.m
@@ -25,8 +25,8 @@ case_desc = 'folding up'; case_fname = 'BfoldingUp'; test_cases{end + 1} = {deg2
 lr = 0.1;
 %method_name = 'J Inverse'; method_fname = 'JInv'; step_function = @(ang, tw) J_inverse_kinematics_step(robot, ang, tw, lr);
 rrk0 = '1e-7'; method_name = sprintf('Redundancy Resolution (%s)', rrk0); method_fname = sprintf('RedRes%s', rrk0); step_function = @(ang, tw) redundancy_resolution_inverse_kinematics_step(robot, ang, tw, lr, str2double(rrk0), 0.01);
-%method_name = 'DLS'; method_fname = 'DLS'; step_function = @(ang, tw) DLS_inverse_kinematics_step(robot, ang, tw, lr, 0.10);
 %step_function = @(ang, tw, dest_T) J_transpose_inverse_kinematics_step(robot, ang, tw, dest_T, lr);
+%method_name = 'DLS'; method_fname = 'DLS'; step_function = @(ang, tw) DLS_inverse_kinematics_step(robot, ang, tw, lr, .1);
 plot_subtitle = sprintf('%s, lr=%.3f, uncapped', case_desc, lr); %%%capped \\pi/8', case_desc, lr);
 video_fname = sprintf('video_%s_%s_uncapped_lr%4.2f.mp4', method_fname, case_fname, lr);
 
