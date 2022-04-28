2c5b50876fdc8350ca998472ad8b0ab63e15daa1
YuewanSun <yuewansun@utexas.edu>
2022-04-28 11:11:30 -0500

    add comment to PA2

--- THA3_1/PA2.m
+++ THA3_1/PA2.m
@@ -1,7 +1,7 @@
 clear;
 [q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion();
 
-
+% get E and S matrix of each frame from quaternion
 for i=1:10
     Rot_E=q2rot(q_Robot_config(i,:));
     p_E=transpose(t_Robot_config(i,:));
@@ -13,6 +13,7 @@ for i=1:10
 
 end
 
+% get A and B matrix from S and E
 for i=1:9
     A(:,:,i) = inv(E(:,:,i))*E(:,:,i+1);
     B(:,:,i) = S(:,:,i)*inv(S(:,:,i+1));
@@ -21,8 +22,8 @@ for i=1:9
     q_A(:,i)=rot2q(RA);
     q_B(:,i)=rot2q(RB);
     M(4*i-3:4*i,:)=get_M(q_A(:,i),q_B(:,i));
-
 end
+% use SVD method to get quarternion
 [U,S0,V] = svd(M);
 Rx=q2rot(V(:,4));
 
@@ -33,15 +34,12 @@ for i=1:9
     A_2(3*i-2:3*i,:)=RA-eye(3,3);
     B_2(3*i-2:3*i,1)=Rx*pB-pA;
 end
-
+% use least square method to get px
 px=inv(transpose(A_2)*A_2)*transpose(A_2)*B_2;
 X=[Rx,px;0 0 0 1]
 
-%%
-Rot_E=q2rot(q_Robot_config(1,:))
-rot=rot2q(Rot_E)
-%rot2q?????
-%%
+
+%% use 10 noisy data
 clear;
 [q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion_noisy();
 for i=1:10
@@ -80,7 +78,7 @@ end
 
 px=inv(transpose(A_2)*A_2)*transpose(A_2)*B_2;
 X=[Rx,px;0 0 0 1]
-%%
+%% use 5 noisy data
 clear;
 [q_Robot_config, q_camera_config,t_Robot_config,t_camera_config ]=data_quaternion_noisy();
 for i=1:5
