ff1774dedaf2d31511dfcf8a3747a0d005d790b6
Jeff Bonyun <jbonyun@utexas.edu>
2022-05-08 10:16:08 -0500

    Expand, but not finish, move_with_joint_limits for part a

--- THA4/move_with_joint_limits.m
+++ THA4/move_with_joint_limits.m
@@ -14,7 +14,35 @@ function [joint_steps] = move_with_joint_limits(robot, start_angles, destTs)
     %               m is a number of steps
     %               k is the number of joints
 
-     % Apply FK to find end effector position in space frame.
-     % This is the vector t from the notes.
+    % Apply FK to find end effector position in space frame.
     startTs = FK_space(robot, start_angles);
-    
+    % Get t vector (translation of tip wrt space frame).
+    t = trans2translation(startTs);
+    % Get vector pgoal from the destination location.
+    pgoal = trans2translation(destTs);
+
+    Js = J_space(robot, start_angles);
+    Jalpha = Js(1:3, :);
+    Jeps = Js(4:6, :);
+
+    % Joint limit bounds
+    qL = robot.joint_limits(:, 1);
+    qU = robot.joint_limits(:, 2);
+    qLA = -eye(7);
+    qLb = start_angles - qL;
+    qUA = eye(7);
+    qUb = qU - start_angles;
+
+    % Set up the linear least squares problem.
+    %   C,d s.t. we minimize Cx - d
+    %   A,b s.t. we require Ax <= b
+    %x = lsqlin(C,d,A,b);
+    C = -skewsym(t) * Jalpha + Jeps;
+    d = pgoal - t;
+    A = [];
+    b = [];
+    %A = -skewsym(t) * Jalpha + Jeps;
+    %b = 3 + pgoal - t;
+    A = [A; qLA; qUA];
+    b = [b; qLb; qUb];
+    [dq, resnorm, residual, exitflag, output, lambda] = lsqlin(C, d, A, b);
--- THA4/test_part_a.m
+++ THA4/test_part_a.m
@@ -1,15 +1,35 @@
 
+% Make sure you can use the functions in the parent directory.
 addpath('..');
 
 robot_no_tool = robot_iiwa();
-tool_length = 100;
 % Make a new robot struct with the tool added to the end effector
-robot_w_tool = robot_no_tool;
-robot_w_tool.home(3,4) = robot.home(3,4) + tool_length;
+tool_length = 100;
+robot_w_tool = robot_no_tool;  % Copies by value.
+robot_w_tool.home(3,4) = robot.home(3,4) + tool_length;  % Adds tool.
 [robot_w_tool.screw, robot_w_tool.bscrew] = calc_screw(robot_w_tool.dof, robot_w_tool.offset, robot_w_tool.axes, robot_w_tool.home);
 
-start_angles = [3.60426218545767 1.26563657105162 -3.19483613264846 -6.92175875711803 -1.79462324164591 0.672289765429706 2.96056445057525]';
-target_joint_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
-target_Ts  = FK_space(robot_w_tool, target_joint_angles);
+% Choose starting and ending angles.
+% Choose random seed for repeatable randomness.
+rng(12345);
+
+%start_angles = [3.60426218545767 1.26563657105162 -3.19483613264846 -6.92175875711803 -1.79462324164591 0.672289765429706 2.96056445057525]';
+start_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
+start_Ts = FK_space(robot_w_tool, start_angles);
+
+% Target with random pose, but definitely reachable.
+%target_joint_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
+%target_Ts  = FK_space(robot_w_tool, target_joint_angles);
+
+% Target with random deviations in joint angles from start.
+%target_joint_angles = start_angles + rand(robot.dof, 1) .* 0.05;
+%target_Ts  = FK_space(robot_w_tool, target_joint_angles);
+
+% Target with random orientation but only small translation from start.
+target_axis = (rand(3, 1) - 0.5);
+target_axis = target_axis ./ vecnorm(target_axis);
+target_aa = [target_axis; rand(1,1) * 2*pi];
+target_translation = trans2translation(start_Ts) + randn(3, 1) * 2;
+target_Ts = rottranslation2trans(aa2rot(target_aa), target_translation);
 
 [joint_steps] = move_with_joint_limits(robot_w_tool, start_angles, target_Ts);
