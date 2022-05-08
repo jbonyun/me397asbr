41eb480e16dcf4b636ec55acdc9083edb5e3cb0c
Jeff Bonyun <jbonyun@utexas.edu>
2022-05-08 17:56:28 -0500

    Bring part a closer to correct
    Still seems to fail when I include the upper and lower joint limits.

--- THA4/move_with_joint_limits.m
+++ THA4/move_with_joint_limits.m
@@ -1,4 +1,4 @@
-function [joint_steps] = move_with_joint_limits(robot, start_angles, destTs)
+function [dq] = move_with_joint_limits(robot, start_angles, destTs)
     % Calculates a path from startT to destT.
     % Respects the joint limits in the robot struct.
     % Input:
@@ -6,26 +6,27 @@ function [joint_steps] = move_with_joint_limits(robot, start_angles, destTs)
     %               dof: scalar number of joints
     %               joint_limits: dofx2 matrix with min and max
     %                             angles for each joint (rad).
-    % start_angles: 1xk vector of starting joint angles.
-    % destTs:        4x4 target transformation matrix in space frame (which
+    % start_angles: dofx1 vector of starting joint angles.
+    % destTs:       4x4 target transformation matrix in space frame (which
     %               is the robot root).
     % Output:
-    %  joint_steps: mxk matrix of joint angles along the path to destT
-    %               m is a number of steps
-    %               k is the number of joints
+    %  dq:          dofx1 vector of changes in joint angles
 
-    % Apply FK to find end effector position in space frame.
+    % Apply FK to find tool tip pose in space frame.
     startTs = FK_space(robot, start_angles);
     % Get t vector (translation of tip wrt space frame).
     t = trans2translation(startTs);
     % Get vector pgoal from the destination location.
     pgoal = trans2translation(destTs);
 
+    % Split the Jacobian into translation and rotation parts.
     Js = J_space(robot, start_angles);
     Jalpha = Js(1:3, :);
     Jeps = Js(4:6, :);
 
     % Joint limit bounds
+    % This will require qLA*x <= qLb and qUA*x <= qUb
+    % Which in turn means x >= qLb and x <= qUb
     qL = robot.joint_limits(:, 1);
     qU = robot.joint_limits(:, 2);
     qLA = -eye(7);
@@ -33,16 +34,34 @@ function [joint_steps] = move_with_joint_limits(robot, start_angles, destTs)
     qUA = eye(7);
     qUb = qU - start_angles;
 
+    % Distance from target bounds.
+    % Stay within `max_distance` of target point at all times.
+    % Linearize by turning the sphere surrounding the target point into a
+    % set of polygons.
+    max_distance = 3;
+    m = 10; n = 10;  % Count of polygon mesh; bigger is a better approximation.
+    [polyalpha,polybeta] = meshgrid(linspace(0, 2*pi * (1 - 1/m), m), linspace(0, 2*pi * (1 - 1/n), n));
+    polyalpha = reshape(polyalpha, [], 1);
+    polybeta = reshape(polybeta, [], 1);
+    polyA = [cos(polyalpha).*cos(polybeta) cos(polyalpha).*sin(polybeta) sin(polyalpha) zeros(m*n, 3)];
+    polyA = polyA * Js;  % Change from cartesian to joint space.
+    polyb = repmat(max_distance, m*n, 1);
+
     % Set up the linear least squares problem.
     %   C,d s.t. we minimize Cx - d
     %   A,b s.t. we require Ax <= b
-    %x = lsqlin(C,d,A,b);
+    %  Solves x = lsqlin(C,d,A,b);
     C = -skewsym(t) * Jalpha + Jeps;
-    d = pgoal - t;
+    d = pgoal - t;  % The change in tip location we are seeking.
     A = [];
     b = [];
-    %A = -skewsym(t) * Jalpha + Jeps;
-    %b = 3 + pgoal - t;
-    A = [A; qLA; qUA];
-    b = [b; qLb; qUb];
+    A = [A; polyA]; b = [b; polyb];  % Include polygon mesh limit.
+    %A = [A; qLA; qUA]; b = [b; qLb; qUb];  % Include joint limits.
+
+    % Solve the optimization problem.
     [dq, resnorm, residual, exitflag, output, lambda] = lsqlin(C, d, A, b);
+
+
+    % Did that work?
+
+    
--- THA4/test_part_a.m
+++ THA4/test_part_a.m
@@ -1,7 +1,11 @@
+%%% Test the code for part a.
+
+clear;
 
 % Make sure you can use the functions in the parent directory.
 addpath('..');
 
+%%% Prepare a robot struct
 robot_no_tool = robot_iiwa();
 % Make a new robot struct with the tool added to the end effector
 tool_length = 100;
@@ -9,11 +13,12 @@ robot_w_tool = robot_no_tool;  % Copies by value.
 robot_w_tool.home(3,4) = robot.home(3,4) + tool_length;  % Adds tool.
 [robot_w_tool.screw, robot_w_tool.bscrew] = calc_screw(robot_w_tool.dof, robot_w_tool.offset, robot_w_tool.axes, robot_w_tool.home);
 
-% Choose starting and ending angles.
+%%% Choose starting and ending angles.
+
 % Choose random seed for repeatable randomness.
 rng(12345);
 
-%start_angles = [3.60426218545767 1.26563657105162 -3.19483613264846 -6.92175875711803 -1.79462324164591 0.672289765429706 2.96056445057525]';
+% Choose random joint angles within joint limits.
 start_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
 start_Ts = FK_space(robot_w_tool, start_angles);
 
@@ -26,10 +31,13 @@ start_Ts = FK_space(robot_w_tool, start_angles);
 %target_Ts  = FK_space(robot_w_tool, target_joint_angles);
 
 % Target with random orientation but only small translation from start.
+% Because this method is designed for small motions.
 target_axis = (rand(3, 1) - 0.5);
 target_axis = target_axis ./ vecnorm(target_axis);
 target_aa = [target_axis; rand(1,1) * 2*pi];
 target_translation = trans2translation(start_Ts) + randn(3, 1) * 2;
 target_Ts = rottranslation2trans(aa2rot(target_aa), target_translation);
+starting_error = vecnorm(trans2translation(target_Ts) - trans2translation(start_Ts));
 
-[joint_steps] = move_with_joint_limits(robot_w_tool, start_angles, target_Ts);
+% Solve for the required change in joint angles to reach target.
+[dq] = move_with_joint_limits(robot_w_tool, start_angles, target_Ts);
