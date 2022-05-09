726b8c20d869440a5631c921404671ec50298cb0
Jeff Bonyun <jbonyun@utexas.edu>
2022-05-08 21:54:58 -0500

    Fix the test script
    It had some out of date variable names.

--- THA4/test_part_a.m
+++ THA4/test_part_a.m
@@ -10,7 +10,7 @@ robot_no_tool = robot_iiwa();
 % Make a new robot struct with the tool added to the end effector
 tool_length = 100;
 robot_w_tool = robot_no_tool;  % Copies by value.
-robot_w_tool.home(3,4) = robot.home(3,4) + tool_length;  % Adds tool.
+robot_w_tool.home(3,4) = robot_no_tool.home(3,4) + tool_length;  % Adds tool.
 [robot_w_tool.screw, robot_w_tool.bscrew] = calc_screw(robot_w_tool.dof, robot_w_tool.offset, robot_w_tool.axes, robot_w_tool.home);
 
 %%% Choose starting and ending angles.
@@ -19,15 +19,15 @@ robot_w_tool.home(3,4) = robot.home(3,4) + tool_length;  % Adds tool.
 rng(12345);
 
 % Choose random joint angles within joint limits.
-start_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
+start_angles = rand(robot_w_tool.dof, 1) .* (robot_w_tool.joint_limits(:,2) - robot_w_tool.joint_limits(:,1)) + robot_w_tool.joint_limits(:,1);
 start_Ts = FK_space(robot_w_tool, start_angles);
 
 % Target with random pose, but definitely reachable.
-%target_joint_angles = rand(robot.dof, 1) .* (robot.joint_limits(:,2) - robot.joint_limits(:,1)) + robot.joint_limits(:,1);
+%target_joint_angles = rand(robot_w_tool.dof, 1) .* (robot_w_tool.joint_limits(:,2) - robot_w_tool.joint_limits(:,1)) + robot_w_tool.joint_limits(:,1);
 %target_Ts  = FK_space(robot_w_tool, target_joint_angles);
 
 % Target with random deviations in joint angles from start.
-%target_joint_angles = start_angles + rand(robot.dof, 1) .* 0.05;
+%target_joint_angles = start_angles + rand(robot_w_tool.dof, 1) .* 0.05;
 %target_Ts  = FK_space(robot_w_tool, target_joint_angles);
 
 % Target with random orientation but only small translation from start.
