61c7030e85e876e4d04b2c9d979fa297300f0753
YuewanSun <yuewansun@utexas.edu>
2022-05-11 12:56:46 -0500

    Ls_opt3

--- THA4/Ls_opt3.m
+++ THA4/Ls_opt3.m
@@ -1,23 +1,31 @@
-function [dq] = Ls_opt(robot_no_tool, start_angles, Z,center,new_goal)
+function [dq] = Ls_opt3(robot_no_tool, start_angles, destTs,Z,normvector,P0,d)
+%define an infinte plane
+%normvector 1*3
+%P0 point on the plane 3*1
+%d scalar distance
 
     startTs = FK_space(robot_no_tool, start_angles);
-    % Get t vector (translation of tip wrt space frame).
     Ptip=[eye(3),Z;0 0 0 1];
     t = trans2translation(startTs*Ptip);
+    R= trans2rot(startTs);
     % Get vector pgoal from the destination location.
-    pgoal = new_goal;
+    pgoal = trans2translation(destTs);
     Js = J_space(robot_no_tool, start_angles);
     Jalpha = Js(1:3, :);
     Jeps = Js(4:6, :);
+    Jb = J_body(robot_no_tool, start_angles);
+    Jbalpha = Jb(1:3, :);
+    Jbeps = Jb(4:6, :);
+
 
     qLb=[-0.1;-0.1;-0.1;-0.1;-0.1;-0.1;-0.1];
     qUb=[0.1;0.1;0.1;0.1;0.1;0.1;0.1];
     
-    delt=t-center;
+    delt=t-pgoal;
     
     
     
-    max_distance = 3;
+    max_distance = 20000;
     m = 10; n = 10;  % Count of polygon mesh; bigger is a better approximation.
     [polyalpha,polybeta] = meshgrid(linspace(0, 2*pi * (1 - 1/m), m), linspace(0, 2*pi * (1 - 1/n), n));
     polyalpha = reshape(polyalpha, [], 1);
@@ -27,19 +35,23 @@ function [dq] = Ls_opt(robot_no_tool, start_angles, Z,center,new_goal)
     polyA =polyA*Jeps;
     qL = robot_no_tool.joint_limits(:, 1);
     qU = robot_no_tool.joint_limits(:, 2);
-   
     
-    a=0.3;
-    b=0.7;
+    a=0.7;
+    b=0.3;
     C1= -skewsym(t) * Jalpha + Jeps;
     d1=pgoal - t; 
-    C2=eye(7);
-    d2=[0;0;0;0;0;0;0];
+    C2=-skewsym(R*Z)*Jalpha;
+    d2=[0;0;0];
+    planA=-normvector*R*(-skewsym(Z)*Jbalpha+Jbeps);
+    planb=normvector*t-normvector*P0-d;
+
 
     C = [a*C1;b*C2];
     d = [a*d1;b*d2];
-    A=[polyA;eye(7);-eye(7)];
-    b=[polyb;qU-start_angles;start_angles-qL];
+    A=[polyA;eye(7);-eye(7);planA];
+    b=[polyb;qU-start_angles;start_angles-qL;planb];
+    %A=[polyA;eye(7);-eye(7)];
+    %b=[polyb;qU-start_angles;start_angles-qL];
     Aeq=[];
     beq=[];
     dq=lsqlin(C,d,A,b,Aeq,beq,qLb,qUb);
--- THA4/test_part_1.m
+++ THA4/test_part_1.m
@@ -25,80 +25,21 @@ while Dis> 0.1
  
  dq=Ls_opt(robot_no_tool, start_angles, Z,center,p_goal)
  q=q+dq;
- tip_TS=FK_space(robot_w_tool,q);
+ tip_TS=FK_space(robot_no_tool,q);
  p_tip = trans2translation(tip_TS);
  Dis=norm(p_tip-p_goal)
 end
 
 
-
 %%
-    start_angles=[0.2;0.2;0.2;0.2;0.2;0.2;0.2];
-    startTs = FK_space(robot_w_tool, start_angles);
-    p_goal=[100;10;1100];
-    target_Ts=[eye(3,3),p_goal; 0,0,0,1];
-    % Get t vector (translation of tip wrt space frame).
-    t = trans2translation(startTs);
-    % Get vector pgoal from the destination location.
-    pgoal = trans2translation(target_Ts);
-    Js = J_space(robot_w_tool, start_angles);
-    Jalpha = Js(1:3, :);
-    Jeps = Js(4:6, :);
-
-    qLb=[-0.1;-0.1;-0.1;-0.1;-0.1;-0.1;-0.1];
-    qUb=[0.1;0.1;0.1;0.1;0.1;0.1;0.1];
-    
-    delt=t-pgoal;
-    
-    
-    
-    max_distance = 3;
-    m = 10; n = 10;  % Count of polygon mesh; bigger is a better approximation.
-    [polyalpha,polybeta] = meshgrid(linspace(0, 2*pi * (1 - 1/m), m), linspace(0, 2*pi * (1 - 1/n), n));
-    polyalpha = reshape(polyalpha, [], 1);
-    polybeta = reshape(polybeta, [], 1);
-    polyA = [cos(polyalpha).*cos(polybeta) cos(polyalpha).*sin(polybeta) sin(polyalpha)];
-    polyb = repmat(max_distance, m*n, 1)-polyA*delt;
-    polyA =polyA*Jeps;
-    
-
-    C = -skewsym(t) * Jalpha + Jeps;
-    d = pgoal - t; 
-    A=polyA;
-    b=polyb;
-    Aeq=[];
-    beq=[];
-    dq=lsqlin(C,d,A,b,Aeq,beq,qLb,qUb)
-
-%%
-clear;
-
-% Make sure you can use the functions in the parent directory.
-addpath('..');
-
-%%% Prepare a robot struct
-robot_no_tool = robot_iiwa();
-% Make a new robot struct with the tool added to the end effector
-tool_length = 100;
-robot_w_tool = robot_no_tool;  % Copies by value.
-robot_w_tool.home(3,4) = robot_no_tool.home(3,4) + tool_length;  % Adds tool.
-[robot_w_tool.screw, robot_w_tool.bscrew] = calc_screw(robot_w_tool.dof, robot_w_tool.offset, robot_w_tool.axes, robot_w_tool.home);
-
-%start_angles = rand(robot_w_tool.dof, 1) .* (robot_w_tool.joint_limits(:,2) - robot_w_tool.joint_limits(:,1)) + robot_w_tool.joint_limits(:,1);
-start_angles=[0.2;0.2;0.2;0.2;0.2;0.2;0.2];
-start_Ts = FK_space(robot_w_tool, start_angles)
-start_Ts2=FK_space(robot_no_tool,start_angles);
-Z=[0;0;100];
-Ptip=[eye(3),Z;0 0 0 1];
-t=start_Ts2*Ptip
-%%
+clear
 addpath('..');
 
 %%% Prepare a robot struct
 robot_no_tool = robot_iiwa();
 
 start_angles=[0.2;0.2;0.2;0.2;0.2;0.2;0.2];
-
+start_Ts = FK_space(robot_no_tool, start_angles);
 p_goal=[100;10;1100];
 target_Ts=[eye(3,3),p_goal; 0,0,0,1];
 Z=[0;0;100];
@@ -115,4 +56,35 @@ while Dis> 0.1
  p_tip = trans2translation(tip_TS*Ptip);
  Dis=norm(p_tip-p_goal)
 end
+
+
+%%
+clear
+addpath('..');
+
+%%% Prepare a robot struct
+robot_no_tool = robot_iiwa();
+
+start_angles=[0.2;0.2;0.2;0.2;0.2;0.2;0.2];
+start_Ts = FK_space(robot_no_tool, start_angles);
+p_goal=[100;10;1100];
+target_Ts=[eye(3,3),p_goal; 0,0,0,1];
+Z=[0;0;100];
+Ptip=[eye(3),Z;0 0 0 1];
+q=start_angles;
+p_tip = trans2translation(start_Ts*Ptip);
+Dis=norm(p_tip-p_goal)
+normvector=[0 1 0];
+P0=[100;0;1100];
+d=5;
+while Dis> 0.1
+ 
+ [dq] = Ls_opt3(robot_no_tool, q, target_Ts,Z,normvector,P0,d)
+ q=q+dq;
+ tip_TS=FK_space(robot_no_tool,q);
+ p_tip = trans2translation(tip_TS*Ptip);
+ Dis=norm(p_tip-p_goal)
+end
+
+
 
