8c8c6db468a46b198f49dc5271e8b947851e44c2
Jeff Bonyun <jbonyun@utexas.edu>
2023-01-16 16:25:19 -0600

    Allow optional epsilon precision for isrot

--- isrot.m
+++ isrot.m
@@ -1,9 +1,12 @@
-function b = isrot(rot)
+function b = isrot(rot, eps)
     % Returns logical 1/0 for whether rot is a valid rotation matrix.
+    if nargin < 2
+        eps = 1e-7;
+    end
 
     dim = ismatrix(rot);
     szr = size(rot,1) == 3;
     szc = size(rot,2) == 3;
-    det1 = logical(abs(det(rot) - 1) < 1e-7);
-    i = iseye(rot * rot');
+    det1 = logical(abs(det(rot) - 1) < eps);
+    i = iseye(rot * rot', eps);
     b = dim && szr && szc && det1 && i;
